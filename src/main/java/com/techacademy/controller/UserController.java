b63bb29ce0900cbc281cf8a60145b4a3c5ba4908
TakenokoSato <kosuke060531@yahoo.co.jp>
2023-03-23 00:26:27 +0900

    課題　初提出

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.techacademy.controller;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
@@ -37,9 +43,47 @@ public class UserController {
 
     /** User登録処理 */
     @PostMapping("/register")
-    public String postRegister(User user) {
+    public String postRegister(@Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+            return getRegister(user);
+        }
         // User登録
         service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+
+    /** User更新画面を表示 */
+    @GetMapping("/update/{id}")
+    public String getUser(User user,@PathVariable("id") Integer id, Model model) {
+        if(id != null) {
+            // Modelに登録
+            model.addAttribute("user", service.getUser(id));
+        }else {
+            // Modelに登録
+            model.addAttribute("user", user);
+        }
+        // User更新画面に遷移
+        return "user/update";
+    }
+
+    /** User更新処理 */
+    @PostMapping("/update/{id}")
+    public String postUser(@Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+         return getUser(user,null,model);
+        }
+        // User登録
+        service.saveUser(user);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+
+    /** User削除処理 */
+    @PostMapping(path="list", params="deleteRun")
+    public String deleteRun(@RequestParam(name="idck") Set<Integer> idck,Model model) {
+        // Userを一括削除
+        service.deleteUser(idck);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,6 +1,8 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,10 +23,23 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
     /** Userの登録を行なう */
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
+    }
+
+    /** Userの削除を行なう */
+    @Transactional
+    public void deleteUser(Set<Integer> idck) {
+        for(Integer id:idck) {
+            userRepository.deleteById(id);
+        }
     }
 
 }
